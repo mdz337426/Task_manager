b6193868d02dc03daf7b092eb1299ae8ce43ca9e
Zeeshan <mdz31157@gmail.com>
2025-02-05 04:03:12 +0530

    some minor modifications

--- main.py
+++ main.py
@@ -14,83 +14,116 @@ db = sqlite3.connect(db_name)
 cursor = db.cursor()
 
 # Model Tune
-model.model_setup(cursor,db)
+model.model_setup(cursor, db)
 
 # Argument Parser 
 parser = argparse.ArgumentParser(description="Secure CLI Login")
 parser.add_argument("command", choices=["signup", "login"], help="Choose to signup or login")
 parser.add_argument("--username", required=True, help="Your username")
-args = parser.parse_args()
+argg = parser.parse_args()
 
 # Auth setup
-if args.command == "signup":
-    auth.signup(args.username,cursor,db)
-    print(f"\n✅ Signup successful! Welcome, {args.username}.")
-elif args.command == "login":
-    auth.login(args.username, cursor)
-    print(f"\n✅ Login successful! Welcome back, {args.username}.!!!\n")
+if argg.command == "signup":
+    auth.signup(argg.username, cursor, db)
+    print(f"\n✅ Signup successful! Welcome, {argg.username}.")
+elif argg.command == "login":
+    auth.login(argg.username, cursor)
+    print(f"\n✅ Login successful! Welcome back, {argg.username}.!!!\n")
 
 # crud operation setup
-crudlist = crud.crud_list(args.username, cursor, db)
-
+crudlist = crud.crud_list(argg.username, cursor, db)
 
 listname = None
 crud_task = None
 
+# Command functions
+def clear_screen(args):
+    subprocess.run(["clear"])
+
+def show_list(args):
+    fmt.printlist(crudlist.show_list())
+
+def add_list(args):
+    crudlist.Add_list(args)
+
+def update_list(args):
+    crudlist.update_list(args[0], args[1])
+
+def remove_list(args):
+    crudlist.remove_list(args)
+
+def use_list(args):
+    global listname, crud_task
+    listname = args[0]
+    crud_task = crud.crudtask(cursor, db, argg.username, listname)
+
+def show_task(args):
+    if listname is None:
+        print("please select the list first")
+        return
+    lst = crud_task.show_task()
+    fmt.printtask(lst)
+
+def add_task(args):
+    if listname is None:
+        print("please select the list first")
+        return
+    crud_task.Add_task(args)
+
+def update_task(args):
+    if listname is None:
+        print("please select the list first")
+        return
+    crud_task.update_task(args[0], args[1])
+
+def remove_task(args):
+    if listname is None:
+        print("please select the list first")
+        return
+    crud_task.remove_task(args)
+
+def update_status(args):
+    if listname is None:
+        print("please select the list first")
+        return
+    crud_task.update_status(args[0], args[1])
+
+def exit_app(args):
+    print("Byee..")
+    global running
+    running = False
+
+# Command map
+commands = {
+    "clear": clear_screen,
+    "help": manual.Help_for_user,
+    "Show_list": show_list,
+    "Add_list": add_list,
+    "Update_list": update_list,
+    "Remove_list": remove_list,
+    "use": use_list,
+    "Show_task": show_task,
+    "Add_task": add_task,
+    "Update_task": update_task,
+    "Remove_task": remove_task,
+    "Update_status": update_status,
+    "exit": exit_app
+}
+
 # REPL console
-manual.welcome_msg(args.username)
-while True:
+manual.welcome_msg(argg.username)
+running = True
+while running:
     print(">>> ", end="")
     userInput = input().split()
-    if len(userInput)==0:
+    if len(userInput) == 0:
         continue
     command = userInput[0]
+    args = userInput[1:]
 
-    if command=="clear":
-        subprocess.run(["clear"])
-    elif command == "help":
-        manual.Help_for_user()
-    elif command == "Show_list":
-        fmt.printlist(crudlist.show_list())
-    elif command == "Add_list":
-        crudlist.Add_list(userInput[1:])
-    elif command == "Update_list":
-        crudlist.update_list(userInput[1], userInput[2])
-    elif command == "Remove_list":
-        crudlist.remove_list(userInput[1:])
-    elif command == "use":
-        listname = userInput[1]
-        crud_task = crud.crudtask(cursor, db, args.username, listname)
-    elif command == "Show_task":
-        if listname is None:
-            print("please select the list first")
-            continue
-        lst = crud_task.show_task()
-        fmt.printtask(lst)
-    elif command == "Add_task":
-        if listname is None:
-            print("please select the list first")
-            continue
-        crud_task.Add_task(userInput[1:])
-    elif command == "Update_task":
-        if listname is None:
-            print("please select the list first")
-            continue
-        crud_task.update_task(userInput[1], userInput[2])
-    elif command == "Remove_task":
-        if listname is None:
-            print("please select the list first")
-            continue
-        crud_task.remove_task(userInput[1:])
-    elif command == "Update_status":
-        if listname is None:
-            print("please select the list first")
-            continue
-        crud_task.update_status(userInput[1], userInput[2])
-    elif command == 'exit':
-        print("Byee..")
-        break
+    if command in commands:
+        commands[command](args)
     else:
-        print(f"ERROR: {userInput[0]}: command not found")
+        print(f"ERROR: {command}: command not found")
 
 db.close()
--- manual.py
+++ manual.py
@@ -7,7 +7,18 @@ Task Manager is a Command line tool to manage tasks and other functionalities
 Type 'help' for help. Type 'clear' to clear the current input console.
 """)
     
-def Help_for_user():
-    print("""Commands... \nAdd_list <List names...>\nShow_list\nUpdate_list <Current name> <New name>\nRemove_list <List names...>\nuse <List name>\nAdd_task <Task names...>\nShow_task\nUpdate_task <Current name> <New name>\nRemove_task <Task names...>\nUpdate_status <Task name> <status>""")
-     
+def Help_for_user(args):
+    print("""
+    Commands:
+    - Add_list <List names...>: Add one or more lists.
+    - Show_list: Display all lists.
+    - Update_list <Current name> <New name>: Update the name of a list.
+    - Remove_list <List names...>: Remove one or more lists.
+    - Use <List name>: Select a list to work with.
+    - Add_task <Task names...>: Add one or more tasks to the selected list.
+    - Show_task: Display all tasks in the selected list.
+    - Update_task <Current name> <New name>: Update the name of a task.
+    - Remove_task <Task names...>: Remove one or more tasks from the selected list.
+    - exit: Exit the Task Manager.
+    """) 
 
